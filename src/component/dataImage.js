fdcf11eca6af334c6f6addd226e16d9158ac8327
metal.bird.13@gmail.com <metal.bird.13@gmail.com>
2019-01-11 23:46:49 +0700

    change data image

---
+++ src/component/dataImage.js
@@ -0,0 +1,286 @@
+const data = [ // dữ liệu ảnh
+    {
+        src: "img/product1.jpeg", // duong dan
+        alt: "Gemek Premium Thiên đường Bảo Sơn - Mr Dương", // mo ta
+        ProjectName: 'Gemek Premium Thiên đường Bảo Sơn', //ten du an
+        classFilter: 'home'// phan loai
+    },
+    {
+        src: "img/product2.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/product3.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/product4.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'office'
+    },
+    {
+        src: "img/product5.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'office'
+    },
+    {
+        src: "img/product7.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'office'
+    },
+    {
+        src: "img/product8.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'office'
+    },
+    {
+        src: "img/product9.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/product10.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'office'
+    },
+    {
+        src: "img/product11.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/product12.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'office'
+    },
+    {
+        src: "img/product13.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/product14.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/product16.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/product17.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/product18.jpeg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/design1.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/design2.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/design3.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/design4.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/design5.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/design6.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'restaurant'
+    },
+    {
+        src: "img/3D.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_1.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_2.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_3.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_4.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_5.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_6.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_7.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_8.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_9.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_10.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/1102d_tan_mai_11.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_1.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_2.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_3.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_4.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_5.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_6.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_7.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_8.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_9.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_10.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    },
+    {
+        src: "img/biet_thu_bac_ninh_11.jpg",
+        alt: "Project Title",
+        ProjectName: 'Project Name2',
+        classFilter: 'home'
+    }
+]
+
+export default data;
